ecfd6aa342876c6e9b1fac0e5c88e7fe14e9d9b0
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-01 21:36:09 +0530

    nav bar added from boostrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Products from './components/Products/Products';
+import NavScrollExample from './components/Products/nav'
 
 function App() {
   return (
     <div className='App'>
         <container>
+          <NavScrollExample/>
           <Products/>
-          
         </container>
     </div>
   );
