c19559f93f0ffae4631743a21f1928665151c612
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-02 18:18:06 +0530

    impliment responsive design for nav bar

--- src/components/Products/nav.js
+++ src/components/Products/nav.js
@@ -5,6 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import smartLogo from "./assets/icon/smartLogo.png";
+import "./nav.css";
 
 function NavScrollExample() {
   return (
@@ -18,20 +19,19 @@ function NavScrollExample() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{height: "50px" }}
+            style={{ maxHeight: "100vh" }}
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
             <Nav.Link href="#action2">Smartphones</Nav.Link>
             <Nav.Link href="#action3">Gadgets</Nav.Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action3">Instagram</NavDropdown.Item>
               <NavDropdown.Item href="#action4">
-                Another action
+                Whatsapp
               </NavDropdown.Item>
-              <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">
-                Something else here
+                Twiter
               </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#" disabled>
@@ -48,8 +48,8 @@ function NavScrollExample() {
             <Button variant="outline-dark">Search</Button>
           </Form>
           
-          <div className="d-flex justfy-content-center align-item-center h-100 ">
-            <span style={{ marginLeft: '50px', marginRight:'20px' }} className="material-symbols-outlined  fs-2 fw-light">local_mall</span>
+          <div className="d-flex justfy-content-center align-item-center h-100 nav-icons">
+            <span style={{ marginRight:'20px' }} className="material-symbols-outlined  fs-2 fw-light sp-bag">local_mall</span>
             <span style={{ marginRight: '20px' }} class="material-symbols-outlined fs-2 fw-light">account_circle</span>
         </div>
         </Navbar.Collapse>
